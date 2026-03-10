ae6434c6795f6f14ebee9dd93aded543d7d2c4f6
Georg Kunz <kwd@gmx.ch>
2026-03-10 23:53:44 +0100

    Update package list with new tools and comments
    Added new packages: fish, wget, jq, fzf, ack, bat, httpie, and kubernetes-cli. Commented out unused packages.

--- home/packages.nix
+++ home/packages.nix
@@ -3,25 +3,32 @@
   home = {
     packages = with pkgs; [
       # dev tools
+      fish
       curl
-      vim
-      tmux
+      wget
+      jq
       htop
       tree
       ripgrep
       gh
       zoxide
+      fzf
+      ack
+      bat
+      httpie
+      kubernetes-cli
+      
 
       # programming languages
       mise # node, deno, bun, rust, python, etc.
 
       # misc
-      nil
-      biome
-      nixfmt-rfc-style
-      yt-dlp
-      ffmpeg
-      ollama
+      #nil
+      #biome
+      #nixfmt-rfc-style
+      #yt-dlp
+      #ffmpeg
+      #ollama
 
       # fonts
       nerd-fonts.fira-code
